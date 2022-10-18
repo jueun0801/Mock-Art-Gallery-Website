f33bb766775ad9e2aa2461ba7721c0ecdb3c2eb7
jueun0801 <109045625+jueun0801@users.noreply.github.com>
2022-10-17 21:12:39 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment_10._4_ArtGallerty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
